f634fd022296224670f60f4f93ef32ad72c007b7
Lucky Verma <idonthaveemail1af@gmail.com>
2024-01-16 16:38:34 +0530

    done

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for (let select of dropdowns) {
   for (currCode in countryList) {
     let newOption = document.createElement("option");
     newOption.innerText = currCode;
-    newOption.value = currCode;
+    newOption.value = currCode;  
     if (select.name === "from" && currCode === "USD") {
       newOption.selected = "selected";
     } else if (select.name === "to" && currCode === "INR") {
